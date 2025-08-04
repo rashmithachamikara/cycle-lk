24577804deff8dbaddd77c735197a80653eda1aa
lakshanabenayake <lakshanabenayake@gmail.com>
2025-08-04 14:24:06 +0530

    moved common componeents to UI folder

--- frontend-web/src/ui/index.ts
+++ frontend-web/src/ui/index.ts
@@ -4,3 +4,12 @@ export { default as Button } from './Button';
 export { default as ErrorDisplay } from './ErrorDisplay';
 export { default as BackButton } from './BackButton';
 export { default as Breadcrumb } from './Breadcrumb';
+export { default as SearchAndFilters } from './SearchAndFilters';
+
+// Bike-related components
+export { default as BikeCard } from './BikeCard';
+export { default as BikeListItem } from './BikeListItem';
+export { default as BikeGrid } from './BikeGrid';
+
+// Export types for SearchAndFilters
+export type { FilterOption, SortOption, SearchAndFiltersProps } from './SearchAndFilters';
