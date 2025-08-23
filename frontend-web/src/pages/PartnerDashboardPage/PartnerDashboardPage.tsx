cda0eee8bedc8f2407b0cfabf762401b6151e968
lakshanabenayake <lakshanabenayake@gmail.com>
2025-08-23 12:59:40 +0530

    added a loader for notifications page

--- frontend-web/src/pages/PartnerDashboardPage/PartnerDashboardPage.tsx
+++ frontend-web/src/pages/PartnerDashboardPage/PartnerDashboardPage.tsx
@@ -16,9 +16,9 @@ import {
   FileText,
   ArrowRight,
   CreditCard,
-  Package,
   Rows4  
 } from 'lucide-react';
+import { Loader } from '../../ui';
 
 import { 
   bookingService, 
@@ -160,8 +160,7 @@ const PartnerDashboardPage = () => {
         {/* Loading State */}
         {loading && (
           <div className="flex justify-center items-center py-12">
-            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
-            <span className="ml-3 text-gray-600">Loading bookings...</span>
+            <Loader />
           </div>
         )}
 
